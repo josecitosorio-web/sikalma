db67cae1585318bab25070496a98c54e10a850ac
Jose-76825708 <76825708@continental.edu.pe>
2026-06-10 08:31:59 -0500

    intento de cambio del data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO usuario (correo_user, contrasena_user, rol_user, doctor_id) 
+INSERT INTO usuario (correo_user, contrasena_user, rol_user, doctor_id)
 VALUES ('admin@gmail.com', '123', 'ADMIN', null);
